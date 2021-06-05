28862a1e1798021a02ee8c3c29e800b83aaef95b
andreseptian <andre.septian2016@student.uny.ac.id>
2021-06-06 01:40:39 +0700

    fisrt commit

--- src/app/master-barang/master-barang.component.ts
+++ src/app/master-barang/master-barang.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpResponse, HttpEventType } from '@angular/common/http';
 
 import * as wjcCore from '@grapecity/wijmo';
+import {Validators,FormControl,FormGroup,FormBuilder} from '@angular/forms';
 import { SelectItem } from 'primeng/components/common/selectitem';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { BreadcrumbService } from '../breadcrumb.service';
@@ -11,6 +12,8 @@ import { environment } from 'src/environments/environment.prod';
 import { MasterBarangService } from './master-barang.service';
 import { AuthService } from '../auth/auth.service';
 
+
+
 @Component({
   selector: 'app-master-barang',
   templateUrl: './master-barang.component.html',
@@ -19,7 +22,11 @@ import { AuthService } from '../auth/auth.service';
 export class MasterBarangComponent implements OnInit {
 
 
-    //Variable Data
+  //Variable Data
+  userform: FormGroup;
+
+  submitted: boolean;
+  
   dataBarang: any[] = [];
   pcode: string;
   pcode_name: string;
@@ -34,55 +41,63 @@ export class MasterBarangComponent implements OnInit {
     private confirmationService: ConfirmationService,
     private messageService: MessageService,
     private masterBarangService: MasterBarangService,
-    private authService: AuthService
+    private authService: AuthService,
+    private fb: FormBuilder
   ) {
-  this.breadCrumbService.setItems([
+    this.breadCrumbService.setItems([
       { label: 'Master Barang' }
     ]);
-   }
-
-  ngOnInit() {
-    this.getDataBarang();
   }
 
-  getDataBarang(){
+  ngOnInit() {
+   
+    this.getDataBarang();
+     this.userform = this.fb.group({
+            'pcode': new FormControl('', Validators.required),
+            'pcode_name': new FormControl('', Validators.required),
+            'price': new FormControl('', Validators.required),
+            'stock': new FormControl('', Validators.required)
+        });
+  }
+
+  getDataBarang() {
     this.progressShow = true;
     this.masterBarangService.getAllBarang().subscribe(
-      (dataBarang)=>{
+      (dataBarang) => {
         this.dataBarang = dataBarang;
       },
-      (error)=>{
+      (error) => {
         this.progressShow = false;
         this.messageService.add({ severity: 'error', summary: 'Error Get data Employee' });
       },
-      () =>{
+      () => {
         this.progressShow = false;
       }
     );
   }
 
-  deleteBarang(pcode: string, stock: string, stk: number=+stock){
+  deleteBarang(pcode: string, stock: string, stk: number = +stock) {
     this.confirmationService.confirm({
       message: 'Are You Sure Want to Delete ?',
       header: 'Delete Confirmation',
       icon: 'ui-icon-warning',
-      accept:()=>{
+      accept: () => {
         this.progressShow = true;
         this.masterBarangService.deleteBarangByPcode(pcode).subscribe(
           (data) => {
-             if (stk >= 0) {
-               this.messageService.add({ severity: 'success', summary: 'Delete barang berhasil' });
+            if (stk >= 0) {
+              this.messageService.add({ severity: 'success', summary: 'Delete barang berhasil' });
             } else {
-               this.messageService.add({ severity: 'error', summary: 'Stock Masih ada' });
-               this.progressShow = false;
+              this.messageService.add({ severity: 'error', summary: 'Stock Masih ada' });
+              this.progressShow = false;
             }
             this.dataBarang = data;
           },
           (error) => {
-            this.messageService.add({ severity: 'error', summary: 'Error Delete data Barang' });   
+            this.messageService.add({ severity: 'error', summary: 'Error Delete data Barang' });
             this.progressShow = false;
           },
-          () =>{
+          () => {
             this.progressShow = false;
           }
         );
@@ -90,27 +105,22 @@ export class MasterBarangComponent implements OnInit {
     });
   }
 
-  addBarang() {
+  addBarang(value: string) {
+
     this.confirmationService.confirm({
       message: 'Are You Sure Want to Add ?',
       header: 'Add Confirmation',
       icon: 'ui-icon-warning',
-      accept:()=>{
+      accept: () => {
         this.progressShow = true;
         this.masterBarangService.insertBarang(this.pcode, this.pcode_name, this.price, this.stock).subscribe(
           () => {
             this.getDataBarang();
             this.messageService.add({ severity: 'success', summary: 'Success Add Barang' });
           },
-          (error)=>{     
-            if (error.status == 500) {
-              this.messageService.add({ severity: 'error', summary: 'pcode sudah ada' });
-              this.messageService.add({ severity: 'error', summary: 'Error Delete data Barang' });
-              this.progressShow = false;
-            } else {
-              this.messageService.add({ severity: 'error', summary: 'Error Delete data Barang' });
-              this.progressShow = false;
-            }
+          (error) => {
+            this.messageService.add({ severity: 'error', summary: 'Error Add data Barang' });
+            this.progressShow = false;
           },
           () => {
             this.progressShow = false;
@@ -118,6 +128,44 @@ export class MasterBarangComponent implements OnInit {
         );
       }
     });
+    //  if (pcode == null && pcode_name == null && price == null && stock == null) {
+    //   this.messageService.add({ severity: 'error', summary: 'Input tidak boleh ada yang kosong' });
+    // } else if (this.pcode == pcode) {
+    //   this.messageService.add({ severity: 'error', summary: 'pcode sudah ada' });
+    // } else {
+    // }
   }
+
+  get diagnostic() { return JSON.stringify(this.userform.value); }
+
+  updateData(stock: string, stk: number = +stock) {
+    if (stk < 0) {
+      this.messageService.add({ severity: 'error', summary: 'Stok tidak boleh minus' });
+    } else {
+      this.confirmationService.confirm({
+        message: 'Are You Sure Want Update Data ?',
+        header: 'Save Confirmation',
+        icon: 'ui-icon-warning',
+        accept: () => {
+          this.progressShow = true;
+          this.masterBarangService.updateBarang(this.dataBarang).subscribe(
+            (data) => {
+              this.dataBarang = data;
+              this.messageService.add({ severity: 'success', summary: 'Success Update Barang' });
+            },
+            (error) => {
+              this.messageService.add({ severity: 'error', summary: 'Error Update data Barang' });
+              this.progressShow = false;
+            },
+            () => {
+              this.progressShow = false;
+            }
+          );
+        }
+      });
+    }
+  }
+
+
 
 }
--- src/app/master-barang/master-barang.module.ts
+++ src/app/master-barang/master-barang.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppSharedModule } from '../shared/app-shared.module';
 import { MasterBarangComponent} from './master-barang.component';
@@ -13,6 +14,8 @@ import { MessageService } from 'primeng/api';
   declarations: [MasterBarangComponent],
   providers: [MasterBarangService, MessageService],
   imports: [
+    FormsModule,    
+    ReactiveFormsModule, 
     ToastModule,
     AppSharedModule,
     RouterModule.forChild([
